0f484c88d843b5b67df3759ccb85136f40f6586b
Niklas Mårdby <niklas.mardby@gmail.com>
2025-01-08 13:34:31 +0100

    Added Fruit type

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,20 @@
 import "./style.css";
 
+type Fruit = {
+	name: string;
+	id: number;
+	family: string;
+	order: string;
+	genus: string;
+	nutritions: {
+		calories: number;
+		fat: number;
+		sugar: number;
+		carbohydrates: number;
+		protein: number;
+	};
+};
+
 const getAllFruits = async () => {
 	const response = await fetch("/api/all");
 	const data = await response.json();
@@ -8,10 +23,15 @@ const getAllFruits = async () => {
 
 getAllFruits();
 
-const getFruit = async (fruit: string) => {
+const getFruit = async (fruit: string): Promise<Fruit> => {
 	const response = await fetch(`/api/${fruit}`);
-	const data = await response.json();
-	console.log(data);
+	const data = (await response.json()) as Fruit;
+	//console.log(data);
+	return data;
 };
 
-getFruit("banana");
+// getFruit("banana");
+
+const banana = await getFruit("banana");
+
+console.log(banana.name);
